e3bd40cd9d7f952bb19c94ca9d86ff33238638cf
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2023-02-26 18:11:23 +0400

    fix: front deploy

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,9 @@ FROM node:16-alpine
 RUN apk add git bash
 
 WORKDIR /app
-COPY ./app/package.json .
+COPY ./app/package.json /app/
 RUN yarn
 
 ENV PATH /app/node_modules/.bin:$PATH
 
-EXPOSE 5173
 EXPOSE 3000
-
-CMD ["yarn", "run", "dev"]
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,11 +1,22 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import './assets/main.css'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 app.use(createPinia())
+app.use(vuetify)
 
 app.mount('#app')
